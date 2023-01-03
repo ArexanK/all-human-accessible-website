f403fd33759697bd190037128c1fd612297d1fda
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-01-03 14:25:10 +0100

    new changes

--- scripts/new.js
+++ scripts/new.js
@@ -11,28 +11,3 @@ navToggle.addEventListener('click', () => {
 
 
 // slider aanbevelingen
-
-
-const slider = document.querySelector('.slider');
-const sliderInner = document.querySelector('.slider-inner');
-const prevButton = document.querySelector('.slider-prev');
-const nextButton = document.querySelector('.slider-next');
-
-let currentSlide = 0;
-const slideWidth = slider.offsetWidth;
-
-prevButton.addEventListener('click', function () {
-    currentSlide--;
-    if (currentSlide < 0) {
-        currentSlide = 2;
-    }
-    sliderInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-});
-
-nextButton.addEventListener('click', function () {
-    currentSlide++;
-    if (currentSlide > 2) {
-        currentSlide = 0;
-    }
-    sliderInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-});
